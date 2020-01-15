8c8bb7fc0f9a3fdc78ed8c157bf3354032b6d406
Alexander Mills <alexmills24>
2020-01-15 14:18:25 +0000

    displays mines

--- main.js
+++ main.js
@@ -1,21 +1,46 @@
 const grid = document.getElementById("grid");
-let mines = document.getElementById("mine-amount");
-let gridSize = document.getElementById("grid-size");
+let mines = 30;
+let gridSize = 15;
 
-startGame = () => {
-  generateGrid();
+const startGame = () => {
+    generateGrid();
 };
 
-generateGrid = () => {
-  grid.innerHTML = "";
-  for (let i = 0; i < 15; i++) {
-    row = grid.insertRow(i);
-    for (let j = 0; j < 15; j++) {
-      cell = row.insertCell(j);
-      cell.id = `cell-${i}-${j}`;
-      cell.onclick = function () {
-        clickCell(this);
-      };
+const generateGrid = () => {
+    grid.innerHTML = "";
+    for (let i = 0; i < 15; i++) {
+        row = grid.insertRow(i);
+        for (let j = 0; j < 15; j++) {
+            cell = row.insertCell(j);
+            cell.id = `cell-${i}-${j}`;
+            let mine = document.createAttribute("data-mine");
+            mine.value = "false";
+            cell.setAttributeNode(mine);
+        }
     }
-  }
+    addMines();
+    revealMines();
 };
+
+const addMines = () => {
+    let minesPlaced = 0;
+    while (minesPlaced < mines) {
+      let row = Math.floor(Math.random() * gridSize);
+      let column = Math.floor(Math.random() * gridSize);
+      let cell = grid.rows[row].cells[column];
+    //   console.log(cell.getAttribute("data-mine"), cell);
+      if (cell.getAttribute("data-mine") === "false") {
+        cell.setAttribute("data-mine", "true");
+        minesPlaced++;
+      }
+    }
+  };
+
+  const revealMines = () => {
+    for (let i = 0; i < gridSize; i++) {
+      for (let j = 0; j < gridSize; j++) {
+        let cell = grid.rows[i].cells[j];
+        if (cell.getAttribute("data-mine") === "true") cell.className = "mine";
+      }
+    }
+  };
