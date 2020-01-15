e06d9b80459333c22ca0f389e545d49d3816bd64
Alexander Mills <alexmills24>
2020-01-15 14:59:11 +0000

    removed console log

--- main.js
+++ main.js
@@ -3,44 +3,43 @@ let mines = 30;
 let gridSize = 15;
 
 const startGame = () => {
-    generateGrid();
+  generateGrid();
 };
 
 const generateGrid = () => {
-    grid.innerHTML = "";
-    for (let i = 0; i < 15; i++) {
-        row = grid.insertRow(i);
-        for (let j = 0; j < 15; j++) {
-            cell = row.insertCell(j);
-            cell.id = `cell-${i}-${j}`;
-            let mine = document.createAttribute("data-mine");
-            mine.value = "false";
-            cell.setAttributeNode(mine);
-        }
+  grid.innerHTML = "";
+  for (let i = 0; i < 15; i++) {
+    row = grid.insertRow(i);
+    for (let j = 0; j < 15; j++) {
+      cell = row.insertCell(j);
+      cell.id = `cell-${i}-${j}`;
+      let mine = document.createAttribute("data-mine");
+      mine.value = "false";
+      cell.setAttributeNode(mine);
     }
-    addMines();
-    revealMines();
+  }
+  addMines();
+  revealMines();
 };
 
 const addMines = () => {
-    let minesPlaced = 0;
-    while (minesPlaced < mines) {
-      let row = Math.floor(Math.random() * gridSize);
-      let column = Math.floor(Math.random() * gridSize);
-      let cell = grid.rows[row].cells[column];
-    //   console.log(cell.getAttribute("data-mine"), cell);
-      if (cell.getAttribute("data-mine") === "false") {
-        cell.setAttribute("data-mine", "true");
-        minesPlaced++;
-      }
+  let minesPlaced = 0;
+  while (minesPlaced < mines) {
+    let row = Math.floor(Math.random() * gridSize);
+    let column = Math.floor(Math.random() * gridSize);
+    let cell = grid.rows[row].cells[column];
+    if (cell.getAttribute("data-mine") === "false") {
+      cell.setAttribute("data-mine", "true");
+      minesPlaced++;
     }
-  };
+  }
+};
 
-  const revealMines = () => {
-    for (let i = 0; i < gridSize; i++) {
-      for (let j = 0; j < gridSize; j++) {
-        let cell = grid.rows[i].cells[j];
-        if (cell.getAttribute("data-mine") === "true") cell.className = "mine";
-      }
+const revealMines = () => {
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      let cell = grid.rows[i].cells[j];
+      if (cell.getAttribute("data-mine") === "true") cell.className = "mine";
     }
-  };
+  }
+};
